0ed7e74ee8d858109736ab360e24f959bf99f601
manueldeortac <61912644+manueldeortac@users.noreply.github.com>
2021-07-02 10:23:16 -0500

    Update meiligao protocol for capture command result

--- src/main/java/org/traccar/protocol/MeiligaoProtocolDecoder.java
+++ src/main/java/org/traccar/protocol/MeiligaoProtocolDecoder.java
@@ -449,7 +449,7 @@ public class MeiligaoProtocolDecoder extends BaseProtocolDecoder {
             position.setDeviceId(deviceSession.getDeviceId());
 
             if (command == MSG_OUTPUT_CONTROL) {
-                System.out.println("Command result : "+ ( (String) command));
+                System.out.println("Command result : "+ ( (String) MSG_OUTPUT_CONTROL));
                 position.set(Position.KEY_RESULT, command);
             }
 
